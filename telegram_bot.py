2866b2c4d58d2ad6eec0bb893fecb627bc91be5f
Sergey Kislyakov <s.kislyakov84@gmail.com>
2026-08-11 14:11:12 +0300

    add button menu

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,18 +1,45 @@
 import asyncio
 import os
 
-from aiogram import Bot, Dispatcher, Router
+from aiogram import Bot, Dispatcher, Router, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.types import ReplyKeyboardMarkup
+from aiogram.types import KeyboardButton
 from dotenv import load_dotenv
 
 
 router = Router()
 
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Новый вопрос")],
+        [KeyboardButton(text="Сдаться")],
+        [KeyboardButton(text="Мой счёт")],
+    ],
+    is_persistent=True,
+    resize_keyboard=True,
+)
+
 
 @router.message(CommandStart())
 async def handle_start(message: Message):
-    await message.answer("Здравствуйте")
+    await message.answer("Здравствуйте", reply_markup=menu)
+
+
+@router.message(F.text == "Новый вопрос")
+async def handle_new_question(message: Message):
+    await message.answer("Пока здесь будет вопрос")
+
+
+@router.message(F.text == "Сдаться")
+async def handle_surrender(message: Message):
+    await message.answer("Вы сдались")
+
+
+@router.message(F.text == "Мой счёт")
+async def handle_score(message: Message):
+    await message.answer("Ваш счёт: 0")
 
 
 @router.message()
